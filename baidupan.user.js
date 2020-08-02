2e94f8ff67b79f635ef7abb9caf612543179bcd9
XanderYe <XanderYe@outlook.com>
2020-08-02 13:12:22 +0800

    Initial commit

---
+++ baidupan.user.js
@@ -0,0 +1,140 @@
+// ==UserScript==
+// @name         百度云下载链接
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  百度云下载链接
+// @author       XanderYe
+// @require      https://lib.baomitu.com/jquery/3.5.0/jquery.min.js
+// @require      https://lib.baomitu.com/snap.svg/0.5.1/snap.svg-min.js
+// @updateURL    https://github.com/XanderYe/xander-tool/raw/master/src/main/resources/js/baidupan.user.js
+// @supportURL   https://www.xanderye.cn/
+// @match        *://pan.baidu.com/*
+// @match        *://yun.baidu.com/*
+// ==/UserScript==
+
+var jQ = $.noConflict(true);
+jQ(function ($) {
+  var parsePanUrl = "http://pan.naifei.cc/?share=" + getShareId();
+  getPwd(function (pwd) {
+    if (pwd) {
+      parsePanUrl = parsePanUrl + "&pwd=" + pwd;
+    }
+    initButton(parsePanUrl);
+  });
+
+
+  function getStrPoint(str) {
+    if (str.length < 2) {
+      return "0:0";
+    }
+
+    var path = "";
+    var current, last = str[0].charCodeAt();
+    var sum = last;
+    for (var i = 1; i < str.length; i++) {
+      current = str[i].charCodeAt();
+      if (i == 1) {
+        path = path + "M";
+      } else {
+        path = path + " L";
+      }
+      path = path + current + " " + last;
+      last = current;
+      sum = sum + current;
+    }
+    path = path + " Z";
+    var index = sum % str.length;
+    var data = getSnap().path.getPointAtLength(path, str[index].charCodeAt());
+    return data.m.x + ":" + data.n.y;
+  }
+
+  function getShareId() {
+    var match;
+
+    match = location.href.match(/share\/init\?surl=([a-z0-9-_]+)/i);
+    if (match) {
+      return match[1];
+    }
+
+    match = location.pathname.match(/\/s\/1([a-z0-9-_]+)/i);
+    if (match) {
+      return match[1];
+    }
+
+    return null;
+  }
+
+  function getPwd(callback) {
+    var shareId = getShareId(location.href);
+    var params = {
+      share_id: shareId,
+      share_source: "baidu",
+      share_point: getStrPoint(shareId),
+      share_link: location.href
+    };
+    var pwd;
+    $.ajax({
+      url: "https://api.newday.me/share/disk/query",
+      type: "POST",
+      data: params,
+      async: false,
+      success: function (data) {
+        if (data.code == 1) {
+          pwd = data.data.share_pwd;
+        }
+        if (callback) {
+          callback(pwd);
+        }
+      },
+      error: function () {
+        if (callback) {
+          callback(pwd);
+        }
+      }
+    })
+  }
+
+  function getSnap() {
+    if (typeof Snap != "undefined") {
+      return Snap;
+    } else {
+      return window.Snap;
+    }
+  }
+
+  function showDialog(body, footer) {
+    var dialog = require("system-core:system/uiService/dialog/dialog.js").verify({
+      title: "度云简易分享链接提取",
+      img: "img",
+      vcode: "vcode"
+    });
+
+    // 内容
+    $(dialog.$dialog).find(".dialog-body").html(body);
+
+    $(dialog.$dialog).find(".dialog-footer").html(footer);
+
+    dialog.show();
+  }
+
+  function require(name) {
+    return unsafeWindow.require(name);
+  }
+
+  function initButton(parsePanUrl) {
+    if ($(".x-button-box").length) {
+      var html = '<a href="' + parsePanUrl + '" target="_blank" class="g-button parse-button"><span class="g-button-right"><em class="icon icon-disk" title="下载"></em><span class="text">解析链接</span></span></a>';
+      $(".x-button-box").append(html);
+    } else {
+      setTimeout(initButton, 500);
+    }
+  }
+
+  function initButtonEvent(parsePanUrl) {
+    $(document).on("click", ".parse-button", function () {
+      var iframe = $("<iframe></iframe>");
+      iframe.attr("src", parsePanUrl);
+      showDialog(iframe, "");
+    });
+  }
+})
---
+++ optimize.user.js
@@ -0,0 +1,94 @@
+// ==UserScript==
+// @name         去广告/去弹窗/优化
+// @namespace    http://tampermonkey.net/
+// @version      1.7
+// @description  去掉各个网站的登录弹窗等辣鸡信息，优化某些功能
+// @author       XanderYe
+// @require      http://lib.baomitu.com/clipboard.js/1.7.1/clipboard.min.js
+// @require      https://lib.baomitu.com/jquery/3.5.0/jquery.min.js
+// @updateURL    https://github.com/XanderYe/xander-tool/raw/master/src/main/resources/js/optimize.user.js
+// @supportURL   https://www.xanderye.cn/
+// @match        http*://www.zhihu.com/question/*
+// @match        http*://*.csdn.net/*
+// @match        http*://www.iqiyi.com/*.html
+// @match        http*://www.bilibili.com/*
+// @match        http*://bbs.hupu.com/*.html
+// @match        http*://jingyan.baidu.com/article/*
+// @match        http*://dnf.qq.com/gift.shtml
+// @match        https://www.pianku.tv/py/*.html
+// ==/UserScript==
+
+var jQ = $.noConflict(true);
+jQ(function($){
+    var newStyle = document.createElement("style");
+    var newNode;
+    if (website("zhihu")) {
+        // 知乎干掉登录弹窗、推荐图书
+        newNode = document.createTextNode("html {overflow: auto !important;} .Modal-backdrop, .MCNLinkCard {display: none !important} .Modal-closeIcon {fill: #8590a6 !important}");
+        // 监控登录窗，干掉2次弹窗
+        var signFlowModal = null;
+        var count = 0;
+        var interval = setInterval(function () {
+            signFlowModal = $("body .signFlowModal");
+            if (signFlowModal.length > 0) {
+                signFlowModal.parent().parent().remove();
+                count++;
+            }
+            if (count == 2) {
+                clearInterval(interval);
+            }
+        },100);
+        // 知乎不加载图片的问题
+        var images = $("img");
+        for(var i = 0; i < images.length; i++) {
+            var src = images.eq(i).attr("src");
+            if(src.indexOf("data") > -1) {
+                images.eq(i).attr("src", images.eq(i).attr("data-actualsrc"));
+            }
+        }
+    } else if (website("csdn")) {
+        // CSDN
+        newNode = document.createTextNode(".login-mark,.login-box,.leftPop,.opt-box {display: none !important} .comment-list-box {max-height: none !important} .htmledit_views code ol li{height: 26px !important}");
+    } else if (website("iqiyi")) {
+        // 爱奇艺
+        newNode = document.createTextNode("div[templatetype=common_pause] {display: none !important}");
+    } else if (website("bilibili")) {
+        // B站
+        newNode = document.createTextNode(".gg-floor-module, #slide_ad, .gg-window .operate-card,.banner-card.b-wrap:nth-of-type(1) {display: none !important}");
+    }  else if (website("hupu")) {
+        // 虎扑去除方向键上下页
+        document.onkeydown = function(){}
+    } else if (website("jingyan.baidu")) {
+        // 关闭百度经验浮动视频
+        $(".video-originate-container").remove();
+    } else if (website("dnf.qq.com")) {
+        newNode = document.createTextNode("#cpybtn {cursor: pointer}")
+        $("#logined").append("<a id='cpybtn'  data-clipboard-text='" + document.cookie + "'>复制cookie</a>");
+        var clipboard = new Clipboard('#cpybtn');
+        clipboard.on('success', function(e) {
+            alert("复制成功");
+        });
+        clipboard.on('error', function(e) {
+            console.log(e);
+        });
+    } else if (website("pianku")) {
+        remove();
+        function remove() {
+            if ($("#pp").length != 0) {
+                adremove();
+            } else {
+                setTimeout(function () {
+                    remove();
+                }, 500);
+            }
+        }
+    }
+    if (newNode !== undefined) {
+        newStyle.appendChild(newNode);
+        document.head.appendChild(newStyle);
+    }
+
+    function website(keyword) {
+        return location.host.indexOf(keyword) > -1;
+    }
+})
