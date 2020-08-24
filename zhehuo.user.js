807a2b43f799415670e3b74fa4b33584ae6855f2
XanderYe <XanderYe@outlook.com>
2020-08-24 14:32:46 +0800

    支持class加随机数

--- zhehuo.user.js
+++ zhehuo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         这货怎么样破解
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  这货怎么样破解收费，不用输入密码就能看
 // @author       XanderYe
 // @require      https://lib.baomitu.com/jquery/3.5.0/jquery.min.js
@@ -15,3 +15,8 @@ var newStyle = document.createElement("style");
 var newNode = document.createTextNode(".denglutishi, .logobq, .zheceng {display: none} .paofenjietu, #chart-wrapper {filter: unset !important} .sy{background-image: unset !important}");
 newStyle.appendChild(newNode);
 document.head.appendChild(newStyle);
+
+var jQ = $.noConflict(true);
+jQ(function ($) {
+  $("div[class^='denglutishi']").css("display", "none");
+})
