3b7ddb70e6cfce00713878d0c975c9c17c677b33
XanderYe <XanderYe@outlook.com>
2020-08-26 13:42:07 +0800

    提交bt-tracker格式化js

---
+++ bttracker.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         格式化输出bt-tracker
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  格式化输出bt-tracker，用于aria2
+// @author       XanderYe
+// @require      https://lib.baomitu.com/jquery/3.5.0/jquery.min.js
+// @updateURL    https://github.com/XanderYe/xander-tool/raw/master/src/main/resources/js/bttracker.user.js
+// @supportURL   https://www.xanderye.cn/
+// @match        https://ngosang.github.io/trackerslist/trackers_best.txt
+// ==/UserScript==
+
+var jQ = $.noConflict(true);
+jQ(function($){
+  var originalData = $("pre").html();
+  var res = originalData.split(/\s+/g);
+  res.pop();
+  $("pre").text(res.join(","));
+})
