e1f809b8a1050b5059e800e6f22ca497883ef46b
XanderYe <XanderYe@outlook.com>
2021-05-06 16:21:41 +0800

    优化脚本

--- youjia.user.js
+++ youjia.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         有驾对比增强
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  有驾对比增强，支持对比列表勾选车型对比
 // @author       XanderYe
 // @require      https://lib.baomitu.com/jquery/3.5.0/jquery.min.js
@@ -10,20 +10,23 @@
 // @match        http*://youjia.baidu.com/view/carTrain*
 // @grant        GM_getValue
 // @grant        GM_setValue
+// @grant        GM_addStyle
 // ==/UserScript==
 
-var jQ = $.noConflict(true);
+let jQ = $.noConflict(true);
 jQ(function($){
 
   addCss();
   init();
 
   function init() {
-    var compareBtn = $(".fixed-box");
+    let compareBtn = $(".fixed-box");
     if (compareBtn.length > 0) {
       compareBtn.bind("click", function () {
         initBtn();
       })
+      bindSelect();
+      bindContrast();
     } else {
       setTimeout(function () {
         init();
@@ -32,64 +35,111 @@ jQ(function($){
   }
 
   function initBtn() {
-    var checkedModels = GM_getValue("checkedModels");
-    var btn = $(".start-btn");
-    var dataV = getAttr(btn, "data-v");
-    getCompareModel(function (data) {
-      var list = data.Result.arrData.list;
-      var selectPDoms = $(".selected .selected-item");
-      for (var i in selectPDoms) {
-        var selectPDom = selectPDoms.eq(i);
-        if (selectPDom.length > 0 && selectPDom.find(".select-box").length == 0) {
-          var modelId = list[i].modelId;
-          var nodes;
-          if (checkedModels && checkedModels.indexOf(modelId) > -1) {
-            nodes = '<span class="c-checkbox--input is-checked"><span class="c-checkbox--inner"></span>' +
-              '<input type="checkbox" class="select-box c-checkbox--original" checked ';
-          } else {
-            nodes = '<span class="c-checkbox--input"><span class="c-checkbox--inner"></span>' +
-              '<input type="checkbox" class="select-box c-checkbox--original" ';
-          }
-          nodes = nodes + 'model-id="' + modelId + '"></span>';
+    initCompareBtn();
+    bindEmpty();
+    initCheckbox();
+  }
+
+  function initCompareBtn() {
+    let btn = $(".start-btn");
+    let dataV = getAttr(btn, "data-v");
+    if ($("#compare-btn").length === 0) {
+      btn.after('<div ' + dataV + ' class="start-btn" id="compare-btn">选择对比</div>');
+      bindCompareBtn();
+    }
+  }
+
+  function initCheckbox() {
+    let checkedModels = GM_getValue("checkedModels");
+    getCompareModel( (data) => {
+      let list = data.Result.arrData.list;
+      let selectPDoms = $(".selected .selected-item");
+      for (let i in selectPDoms) {
+        let selectPDom = selectPDoms.eq(i);
+        if (selectPDom.length > 0 && selectPDom.find(".select-box").length === 0) {
+          let modelId = list[i].modelId;
+          let checked = checkedModels && checkedModels.indexOf(modelId) > -1;
+          let nodes = `
+          <span class="c-checkbox--input ${checked ? 'is-checked' : ''}">
+            <span class="c-checkbox--inner"></span>
+            <input type="checkbox" class="select-box c-checkbox--original" ${checked ? "checked" : ''} model-id="${modelId}"/>
+          </span>
+          `;
           selectPDom.find("p").before(nodes);
         }
       }
-      $(".c-checkbox--inner").unbind().bind("click", function () {
-        var checkBoxDom = $(this).next();
-        if (checkBoxDom.attr("checked")) {
-          checkBoxDom.attr("checked", false);
-          $(this).parent(".c-checkbox--input").removeClass("is-checked");
-        } else {
-          checkBoxDom.attr("checked", true);
-          $(this).parent(".c-checkbox--input").addClass("is-checked");
-        }
-        changeCompareBtnStatus();
-      });
+      bindCheckbox();
+
+      bindSelect();
+
       changeCompareBtnStatus();
     });
-    if ($("#compare-btn").length == 0) {
-      btn.after('<div ' + dataV + ' class="start-btn" id="compare-btn">选择对比</div>');
+  }
+
+  function bindCheckbox() {
+    $(".c-checkbox--inner").unbind().bind("click", function () {
+      let checkBoxDom = $(this).next();
+      if (checkBoxDom.attr("checked")) {
+        checkBoxDom.attr("checked", false);
+        $(this).parent(".c-checkbox--input").removeClass("is-checked");
+      } else {
+        checkBoxDom.attr("checked", true);
+        $(this).parent(".c-checkbox--input").addClass("is-checked");
+      }
       changeCompareBtnStatus();
-      $("#compare-btn").unbind().bind("click", function () {
-        if ($(this).hasClass("disabled")) {
-          return;
-        }
-        var modelIds = [];
-        console.log($(".select-box:checked"));
-        $.each($(".select-box:checked"), function (index, val) {
-          if ($(val).attr("model-id")) {
-            modelIds.push($(val).attr("model-id"));
-          }
-        })
-        if (modelIds.length == 0) {
-          alert("请勾选车型");
-        } else {
-          GM_setValue("checkedModels", modelIds);
-          var open = window.open('_blank');
-          open.location = "https://youjia.baidu.com/view/constrast?type=model&modelIds=" + modelIds.join(",");
-        }
-      })
-    }
+      GM_setValue("checkedModels", getModelIds());
+    });
+  }
+
+  function bindCompareBtn() {
+    $("#compare-btn").unbind().bind("click", function () {
+      if ($(this).hasClass("disabled")) {
+        return;
+      }
+      let modelIds = getModelIds();
+      GM_setValue("checkedModels", modelIds);
+      let open = window.open('_blank');
+      open.location = "https://youjia.baidu.com/view/constrast?type=model&modelIds=" + modelIds.join(",");
+    })
+  }
+
+  function getModelIds() {
+    let modelIds = [];
+    $.each($(".select-box:checked"), (index, val) => {
+      if ($(val).attr("model-id")) {
+        modelIds.push($(val).attr("model-id"));
+      }
+    })
+    return modelIds;
+  }
+
+  function bindEmpty() {
+    $(".rtl h2 span").unbind().bind("click", function () {
+      GM_setValue("checkedModels", []);
+      changeCompareBtnStatus();
+    });
+  }
+
+  function bindContrast() {
+    $(document).on("click", ".list-btn.btn-contrast", function () {
+      let link = $(this).prev();
+      let href = link.attr("href");
+      let hrefs = href.split("/");
+      let modelId = hrefs[4].split("?")[0];
+      let checkedModels = GM_getValue("checkedModels");
+      if (checkedModels && checkedModels.length > 0) {
+        checkedModels.push(modelId);
+      } else {
+        checkedModels = [modelId];
+      }
+      GM_setValue("checkedModels", checkedModels);
+    })
+  }
+
+  function bindSelect() {
+    $(".rtl").on("click", ".content-model .series", function () {
+      console.log("选择");
+    })
   }
 
   function changeCompareBtnStatus() {
@@ -99,20 +149,22 @@ jQ(function($){
       $("#compare-btn").removeClass("disabled");
     }
   }
-  
+
   function getCompareModel(callback) {
     $.ajax({
       url: "/compare/getmodel?token=1_526c1239fc0b0512a2bd13ac6b962f5f",
       success: function (data) {
-        callback(data);
+        if (callback) {
+          callback(data);
+        }
       }
     });
   }
 
   function getAttr(dom, key) {
-    var attrs = dom.attrs();
+    let attrs = dom.attrs();
     if (attrs) {
-      for (var k in attrs) {
+      for (let k in attrs) {
         if (k.indexOf(key) > -1) {
           return k;
         }
@@ -121,16 +173,73 @@ jQ(function($){
   }
 
   function addCss() {
-    var newStyle = document.createElement("style");
-    var newNode = document.createTextNode('.c-checkbox--input {position: relative; vertical-align: middle; line-height: 1}' +
-      '.c-checkbox--input.is-checked .c-checkbox--inner {border-color: #00cecf; background-color: #00cecf}' +
-      '.c-checkbox--input .c-checkbox--inner {display: inline-block; position: relative; border: 1px solid #dcdfe6; border-radius: 2px; -webkit-box-sizing: border-box; box-sizing: border-box; width: 12px; height: 12px; background-color: #fff; z-index: 1; -webkit-transition: border-color .25s cubic-bezier(.71,-.46,.29,1.46),background-color .25s cubic-bezier(.71,-.46,.29,1.46); transition: border-color .25s cubic-bezier(.71,-.46,.29,1.46),background-color .25s cubic-bezier(.71,-.46,.29,1.46);}' +
-      '.c-checkbox--input.is-checked .c-checkbox--inner:after {-webkit-transform: rotate(45deg) scaleY(1); transform: rotate(45deg) scaleY(1);}' +
-      '.c-checkbox--input .c-checkbox--inner:after {-webkit-box-sizing: content-box; box-sizing: content-box; content: ""; border: 1px solid #fff; border-left: 0; border-top: 0; width: 2px; height: 5px; left: 4px; position: absolute; top: 1px; -webkit-transform: rotate(45deg) scaleY(0); transform: rotate(45deg) scaleY(0); -webkit-transition: -webkit-transform .15s ease-in .05s; transition: -webkit-transform .15s ease-in .05s; transition: transform .15s ease-in .05s; transition: transform .15s ease-in .05s,-webkit-transform .15s ease-in .05s; -webkit-transform-origin: center; transform-origin: center;}' +
-      '.c-checkbox--original {position: absolute; outline: none; width: 0; height: 0; margin: 0; opacity: 0; z-index: -1}' +
-      '.selected .selected-item p {margin-left: 10px}');
-    newStyle.appendChild(newNode);
-    document.head.appendChild(newStyle);
+    let style = `
+      .c-checkbox--input {
+        position: relative; 
+        vertical-align: middle; 
+        line-height: 1;
+      }
+      .c-checkbox--input.is-checked .c-checkbox--inner {
+        border-color: #00cecf;
+        background-color: #00cecf;
+       }
+      .c-checkbox--input .c-checkbox--inner {
+        display: inline-block; 
+        position: relative; 
+        border: 1px solid #dcdfe6; 
+        border-radius: 2px; 
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box; 
+        width: 12px; 
+        height: 12px; 
+        background-color: #fff; 
+        z-index: 1;
+        -webkit-transition: border-color .25s cubic-bezier(.71,-.46,.29,1.46),background-color .25s cubic-bezier(.71,-.46,.29,1.46); 
+        transition: border-color .25s cubic-bezier(.71,-.46,.29,1.46),background-color .25s cubic-bezier(.71,-.46,.29,1.46);
+      }
+      .c-checkbox--input.is-checked .c-checkbox--inner:after {
+        -webkit-transform: rotate(45deg) scaleY(1); 
+        transform: rotate(45deg) scaleY(1);
+      }
+      .c-checkbox--input .c-checkbox--inner:after {
+        -webkit-box-sizing: content-box; 
+        box-sizing: content-box; 
+        content: ""; 
+        border: 1px solid #fff; 
+        border-left: 0; 
+        border-top: 0; 
+        width: 2px; 
+        height: 5px; 
+        left: 4px; 
+        position: absolute; 
+        top: 1px; 
+        -webkit-transform: rotate(45deg) scaleY(0); 
+        transform: rotate(45deg) scaleY(0); 
+        -webkit-transition: -webkit-transform .15s ease-in .05s; 
+        transition: -webkit-transform .15s ease-in .05s; 
+        transition: transform .15s ease-in .05s; 
+        transition: transform .15s ease-in .05s,-webkit-transform .15s ease-in .05s; 
+        -webkit-transform-origin: center; 
+        transform-origin: center;
+      }
+      .c-checkbox--original {
+        position: absolute; 
+        outline: none; 
+        width: 0; 
+        height: 0; 
+        margin: 0; 
+        opacity: 0; 
+        z-index: -1;
+      }
+      .selected .selected-item p {
+        margin-left: 10px
+      }
+      
+      .selector .container {
+        z-index: 2;
+      }
+    `
+    GM_addStyle(style);
   }
 
   $.fn.attrs = function() {
