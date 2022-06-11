86528f0e613b66a6b474bbaf65b0d1815ae6ca1d
XanderYe <XanderYe@outlook.com>
2022-06-11 21:55:01 +0800

    fix: 兼容电视剧和综艺

--- progress.user.js
+++ progress.user.js
@@ -17,7 +17,7 @@
   if (host.indexOf("iqiyi") > -1) {
     barNode = document.createTextNode(".iqp-bottom-hide .iqp-progress,.iqp-bottom-hide .iqp-progress .iqp-progress-bar {height: 120% !important;}");
   } else if (host.indexOf("v.qq") > -1) {
-    barNode = document.createTextNode(".plugin_ctrl_txp_bottom.txp_none {display: block !important; bottom: -90px !important}");
+    barNode = document.createTextNode(".txp_autohide .txp_bottom {opacity: 1 !important; bottom: -50px !important} .plugin_ctrl_txp_bottom.txp_none {display: block !important; bottom: -90px !important}");
   } else if (host.indexOf("youku") > -1) {
     var dom = '<div class="h5player-dashboard custom-dashboard" style="z-index:9001"><div class="h5player-progress"><div class="progress-hover-container"><div class="progress-container"><div class="input-range-container progress-input-range custom-range" style="display: block;"><div class="input-range-thumb js-thumb custom-thumb"></div></div><div class="progress-loaded custom-loaded"></div><div class="progress-played custom-played"></div></div></div></div></div></div>';
     $("body").append(dom);
